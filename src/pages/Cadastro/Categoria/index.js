74daaf2ff1903789850c3133c1dd64b3e0247557
Eula <eula.silva.teixeira@gmail.com>
2020-08-02 21:26:56 -0300

    Inicio do back-end

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,64 +1,81 @@
-import React, {useState}from 'react';
-import PageDefault from '../../PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../PageDefault';
 import FormField from '../../../components/FormField';
-
+import Button from '../../../components/Button';
 
 const CadastroCategoria = () => {
-    const [categorias, setCategorias] = useState([]);
-    const valoresIniciais = {
-      nome:'',
-      descricao:'',
-      cor:'#ffffff'
-    }
-    const [dadosFormulario, setDadosFormulario] = useState(valoresIniciais);
+  const [categorias, setCategorias] = useState([]);
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '#ffffff',
+  };
+  const [dadosFormulario, setDadosFormulario] = useState(valoresIniciais);
 
-
-    function handleSubmit(e){
-      e.preventDefault();
-      setDadosFormulario(valoresIniciais)
-      setCategorias([...categorias, dadosFormulario]);
-    }
-
-    function handleChangeInput(target){
-
-      const {name, value} = target;
-
-      setDadosFormulario({
-        ...dadosFormulario,
-        [name]:value
-      })
-    }
-
-    return (
-        <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+  useEffect(() => {
+    const URL = 'http://localhost:3333/categorias';
+   
+    fetch(URL)
+      .then((res) => res.json())
+      .then((resposta) => {
+        setCategorias([...categorias, resposta]);
   
-        <form onSubmit={handleSubmit}>
+      });
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setDadosFormulario(valoresIniciais);
+    setCategorias([...categorias, dadosFormulario]);
+  }
+
+  function handleChangeInput(target) {
+    const { name, value } = target;
+
+    setDadosFormulario({
+      ...dadosFormulario,
+      [name]: value,
+    });
+  }
+
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
   
-         <FormField label="Nome da Categoria:" value={dadosFormulario.nome} onChange={(e) => handleChangeInput(e.target)} name="nome" type="text"/>
+      <form onSubmit={handleSubmit}>
+  
+        <FormField label="Nome da Categoria:" value={dadosFormulario.nome} onChange={(e) => handleChangeInput(e.target)} name="nome" type="text" />
          
-         <FormField type="textarea" label="Descrição:"  name="descricao" value={dadosFormulario.descricao} onChange={(e) => handleChangeInput(e.target)} />
+        <FormField as="textarea" type="textarea" label="Descrição:" name="descricao" value={dadosFormulario.descricao} onChange={(e) => handleChangeInput(e.target)} />
   
-          <FormField type="color" label="Cor:"  name="cor" value={dadosFormulario.cor} onChange={(e) => handleChangeInput(e.target)} />
+        <FormField type="color" label="Cor:" name="cor" value={dadosFormulario.cor} onChange={(e) => handleChangeInput(e.target)} />
   
-          <button type="submit">
-            Cadastrar
-          </button>
-        </form>
+        <Button type="submit">
+          Cadastrar
+        </Button>
+      </form>
         
-        <ul>
-          {categorias.map((categoria, index) => (
-            <li key={`${categoria}${index}`}>{categoria.nome}</li>
-          ))}
-        </ul>
-  
-  
-        <Link to="/">
-          Ir para home
-        </Link>
-      </PageDefault>
-    )
-}
+      <ul>
+        {categorias.map((categoria, index) => (
+     
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={`${categoria}${index}`}>{categoria.nome}</li>
+        ))}
+      </ul>
+
+      {categorias.lenth === 0
+      && (
+      <div>
+        Loading...
+      </div>
+      )}
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
+};
 
 export default CadastroCategoria;
